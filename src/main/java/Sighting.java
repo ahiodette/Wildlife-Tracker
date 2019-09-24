175059d7632b1fd1cac11f9aad83a3c5d960355d
ahiodette <ahiode6@gmail.com>
2019-09-24 11:03:50 +0200

    Getters and constructor for Sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,70 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Sighting {
+
+    private int id;
+    private String Rangername;
+    private String Animalname;
+    private String Health;
+    private String Age;
+    private String Location;
+
+    public Sighting(int id, String rangername, String animalname, String health, String age, String location) {
+        this.id = id;
+        Rangername = rangername;
+        Animalname = animalname;
+        Health = health;
+        Age = age;
+        Location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRangername() {
+        return Rangername;
+    }
+
+    public String getAnimalname() {
+        return Animalname;
+    }
+
+    public String getHealth() {
+        return Health;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    @Override
+    public boolean equals(Object anotherSighting) {
+        if (!(anotherSighting instanceof Animal)) {
+            return false;
+        } else {
+            Sighting newsighting = (Sighting) anotherSighting;
+            return this.getRangername().equals(newsighting.getRangername()) &&
+                    this.getAnimalname() == newsighting.getAnimalname() &&
+                    this.getHealth() == newsighting.getHealth()&&
+                    this.getAge() == newsighting.getAge() &&
+                    this.getLocation() == newsighting.getLocation();
+        }
+    }
+
+    public static List<Sighting> all() {
+        String sql = "SELECT * FROM sighting";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Sighting.class);
+        }
+    }
+
+
+
 }
