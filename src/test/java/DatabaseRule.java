3460f29391704c1abc8da924c850db23879062e5
ahiodette <ahiode6@gmail.com>
2019-09-24 15:31:40 +0200

    Create animal-form template

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -3,6 +3,6 @@ import org.sql2o.Sql2o;
 public class DatabaseRule {
     //@Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", "wecode", "12345");  //Those with linux or windows use two strings for username and password
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "wecode", "12345");  //Those with linux or windows use two strings for username and password
     }
 }
